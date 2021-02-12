9185711487d68e3b3a0312846341b3d87d0d3d88
Roberto Riggio <robertoriggio@Robertos-MBP.home>
2021-02-12 14:09:34 +0100

    Refactoring of subscription loop method

--- lightedge/workers/measrepue/measrepue.py
+++ lightedge/workers/measrepue/measrepue.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2020 Roberto Riggio
+# Copyright (c) 2021 Roberto Riggio
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -40,10 +40,24 @@ class MeasRepUe(Subscription):
                          subscription=subscription)
 
         self.app_id = None
+        self.project_id = None
 
-    def find_project_id(self, ):
-        """Find project id from PLMN."""
+    def loop(self):
+        """Periodic loop."""
 
+        # Both project id and app id are set check if they are valid
+        if self.project_id and self.app_id:
+
+            url = "/projects/%s/apps/%s/callbacks/default" % \
+                (self.project_id, self.app_id)
+
+            req = self.manager.get(url)
+
+            # All ok, we can return
+            if req.status_code == 200:
+                return
+
+        # Look for project id
         plmn = self.subscription['filterCriteriaAssocTri']['ecgi']['plmn']
         plmnid = PLMNID("".join(plmn.values()))
 
@@ -51,49 +65,24 @@ class MeasRepUe(Subscription):
 
         if not req.status_code == 200:
             self.log.error("Unable to find PLMN %s", plmnid)
-            return None
-
-        project_id = None
+            return
 
         for project in req.json().values():
 
             if not project['lte_props']:
                 continue
 
-            current = PLMNID(project['lte_props']['plmnid'])
-
-            if current == plmnid:
-                project_id = uuid.UUID(project['project_id'])
+            if plmnid == PLMNID(project['lte_props']['plmnid']):
+                self.project_id = uuid.UUID(project['project_id'])
                 break
 
-        url = "/projects/%s" % project_id
-        req = self.manager.get(url)
+        req = self.manager.get("/projects/%s" % self.project_id)
 
         if req.status_code != 200:
             self.log.error("Unable to find PLMN %s", plmnid)
-            return None
-
-        return project_id
-
-    def loop(self):
-        """Periodic loop."""
-
-        project_id = self.find_project_id()
-
-        if not project_id:
             return
 
-        url = "/projects/%s/apps/%s" % (project_id, self.app_id)
-        req = self.manager.get(url)
-
-        if req.status_code != 200:
-            self.create_worker(project_id)
-
-        self.create_callback(project_id)
-
-    def create_worker(self, project_id):
-        """Create new worker on empower."""
-
+        # Start worker
         associate_id = \
             self.subscription['filterCriteriaAssocTri']['associateId'][0]
 
@@ -112,10 +101,10 @@ class MeasRepUe(Subscription):
             }
         }
 
-        req = self.manager.post("/projects/%s/apps" % project_id, data)
+        req = self.manager.post("/projects/%s/apps" % self.project_id, data)
 
         if not req.status_code == 201:
-            self.log.error("Unable to create worker, error %u",
+            self.log.error("Unable to start worker, error %u",
                            req.status_code)
             return
 
@@ -123,26 +112,24 @@ class MeasRepUe(Subscription):
 
         self.app_id = location.split("/")[-1]
 
-    def create_callback(self, project_id):
-        """Create new callback."""
-
-        callback = "http://127.0.0.1:8889/rni/v2/subscriptions/%s/ch" % \
-            self.service_id
-
+        # Add callback
         data = {
             "version": "1.0",
             "name": "default",
-            "callback": callback,
+            "callback": "http://127.0.0.1:8889/rni/v2/subscriptions/%s/ch" %
+            self.service_id,
             "callback_type": "rest"
         }
 
-        url = "/projects/%s/apps/%s/callbacks" % (project_id, self.app_id)
+        url = "/projects/%s/apps/%s/callbacks" % (self.project_id, self.app_id)
         req = self.manager.post(url, data)
 
         if not req.status_code == 201:
-            self.log.error("Unable to create callback, error %u",
+            self.log.error("Unable to add callback, error %u",
                            req.status_code)
             return
+
+        self.log.info("Remote worker successfully configured.")
 
 
 def launch(context, service_id, subscription, every=EVERY):
